2fa77c0c3ba9c52da3c8f4f84400bf06d40f4ae5
Julissa <julissa.2606@gmail.com>
2021-08-19 13:18:34 -0500

    update controller post-product

--- controller/products.js
+++ controller/products.js
@@ -20,22 +20,26 @@ const getProducts = (req, res, next) => {
 // POST '/products'
 
 const newProduct = async (req, res, next) => {
-  // try {
-  //   const { name, price } = req.body;
-  //   if(!name || !price) {
-  //     return next(404);
-  //   }
-  // } catch (err) {
+  try {
+    const { name, price } = req.body;
+    if (!name || !price) return next(404);
 
-  // };
+    const findProduct = await Product.findOne({ name: req.body.name });
+    if (findProduct) {
+      return res.status(403).json({
+        message: '(Error) El producto ya se encuentra registrado',
+      });
+    }
 
-  res.send('recibido');
-  const newProduct = new Product(req.body);
-  // eslint-disable-next-line no-console
-  console.log(newProduct);
-  // const productSaved = await newProduct.save();
-
-  // return res.status(200).json(productSaved);
+    const newProduct = new Product(req.body);
+    // eslint-disable-next-line no-console
+    console.log(newProduct);
+    const productSaved = await newProduct.save();
+    res.status(200).json(productSaved);
+    res.send('recibido');
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
