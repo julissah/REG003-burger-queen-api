b4ed4208c71d6f5d47d8e4932aa0d1709b9e6881
Julissa <julissa.2606@gmail.com>
2021-08-23 12:47:54 -0500

    paginate products

--- controller/products.js
+++ controller/products.js
@@ -1,20 +1,48 @@
 const Product = require('../models/product');
+const { pagination } = require('../utils/utils');
 
 // GET '/products'
 const getProducts = (req, res, next) => {
-  const productFind = Product.find({});
+  // const productFind = Product.find({});
+  // const { limit } = req.query;
+  const url = `${req.protocol}://${req.get('host') + req.path}`;
+  const options = {
+    page: parseInt(req.query.page, 10) || 1,
+    limit: parseInt(req.query.limit, 10) || 10,
+  };
+  const productFind = Product.paginate({}, options);
   productFind
     .then((doc) => {
       if (!doc) {
         return next(404);
       }
       if (doc) {
+        const links = pagination(doc, url, options.page, options.limit, doc.totalPages);
+        res.links(links);
         return res.status(200).send(doc);
       }
     })
     .catch((err) => {
       console.error(err);
     });
+};
+
+// GET '/products'
+const testProducts = async (req, res, next) => {
+  try {
+    const url = `${req.protocol}://${req.get('host') + req.path}`;
+    const options = {
+      page: parseInt(req.query.page, 10) || 1,
+      limit: parseInt(req.query.limit, 10) || 10,
+    };
+    const products = await Product.paginate({}, options);
+
+    const links = pagination(products, url, options.page, options.limit, products.totalPages);
+    res.links(links);
+    return res.status(200).json(products.docs);
+  } catch (err) {
+    next(err);
+  }
 };
 
 // POST '/products'
