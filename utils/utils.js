56e97a4ba71269585e8c9c94c1cb6c325a0d666a
CarolAbcl <carolallende@gmail.com>
2021-09-03 12:57:55 -0400

    deploy with heroku

--- utils/utils.js
+++ utils/utils.js
@@ -13,12 +13,6 @@ module.exports.isAValidEmail = (email) => {
   return (emailRegex.test(email));
 };
 module.exports.isAWeakPassword = (password) => ((password.length <= 3));
-// module.exports.isAWeakPassword = (password) => {
-//   if (password.length < 3) {
-//     return false;
-//   }
-//   return true;
-// };
 
 module.exports.isObjectId = (params) => {
   const checkForValidMongoDbID = new RegExp('^[0-9a-fA-F]{24}$');
